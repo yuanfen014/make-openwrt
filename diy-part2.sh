300d4132ac81d3a50770cc53c3527caf839f9e45
yuanfen014 <80026868+yuanfen014@users.noreply.github.com>
2022-03-03 13:31:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,5 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 # Modify default password
 sed -i 's/root::0:0:99999:7:::/root:$1$2pfixn80$LDjAwj4Pbq9I2JSMf83X2.:18700:0:99999:7:::/g' package/base-files/files/etc/shadow
 #Poweroffdevice
-cd openwrt
 curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm
 curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
