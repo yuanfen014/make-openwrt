b5dcb760c2ea7c7873b533b763cd036587ebad3e
yuanfen014 <80026868+yuanfen014@users.noreply.github.com>
2023-01-30 16:51:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,11 @@
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 # Modify default password
 sed -i 's/root::0:0:99999:7:::/root:$1$2pfixn80$LDjAwj4Pbq9I2JSMf83X2.:18700:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefile
+
+
 #Poweroffdevice
 curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm
 curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
