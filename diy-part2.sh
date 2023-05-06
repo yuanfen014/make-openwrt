6067368eb0d3425fdd63eaf7d805f77eb65ab3dd
yuanfen014 <80026868+yuanfen014@users.noreply.github.com>
2023-05-06 13:25:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,7 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 sed -i 's/root::0:0:99999:7:::/root:$1$2pfixn80$LDjAwj4Pbq9I2JSMf83X2.:18700:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 
-sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefile
 
 
 #Poweroffdevice
