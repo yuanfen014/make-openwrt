b47e1f09edcc324479f5efa60d6d35c1eb9007d7
yuanfen014 <80026868+yuanfen014@users.noreply.github.com>
2023-05-11 17:04:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 # Modify default password
-sed -i 's/root::0:0:99999:7:::/root:$1$2pfixn80$LDjAwj4Pbq9I2JSMf83X2.:18700:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's/root:::0:99999:7:::/root:$1$2pfixn80$LDjAwj4Pbq9I2JSMf83X2.:18700:0:99999:7:::/g'  package/base-files/files/etc/shadow
 
 
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefile
